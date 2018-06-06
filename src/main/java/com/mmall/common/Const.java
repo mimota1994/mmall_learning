e0c2ea81a1b698fa3f8d50a3fef7bc8899b28a32
mimota1994 <1643160505@qq.com>
2018-06-06 22:22:21 +0800

    complete cart module

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -18,6 +18,16 @@ public class Const {
         Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_desc","price_asc");
     }
 
+    public interface Cart{
+        int CHECKED = 1;//cart is choosed
+        int UN_CHECKED = 0;
+
+        int EMPTY_CART =0;
+
+        String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
+        String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
+    }
+
     public interface Role{
         int ROLE_CUSTOMER = 0; //common user
         int ROLE_ADMIN = 1; //admin user
